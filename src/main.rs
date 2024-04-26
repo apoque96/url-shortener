b6146303ed23abb92a97b51a262dd373fb356b22
Dereck Cabrera <dereckcabrerang@gmail.com>
2024-04-25 19:48:33 -0600

    Url validation

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use rocket::fs::{relative, FileServer};
 use std::sync::Mutex;
 
 pub mod routes;
-use crate::routes::{index, new_url, Url};
+use crate::routes::{index, new_url, UrlStruct};
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, new_url])
-        .manage(Mutex::new(Url::new()))
+        .manage(Mutex::new(UrlStruct::new()))
         .mount("/", FileServer::from(relative!("static")))
         .attach(rocket_dyn_templates::Template::fairing())
 }
